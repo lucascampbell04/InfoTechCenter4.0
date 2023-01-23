0e9b00a571c0d103b8ba108aeb78da6403258a89
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-01-23 14:41:07 -0500

    Add print lines

---
+++ DeclarationOfIndependence.py
@@ -0,0 +1,17 @@
+# Programmer: Lucas Campbell
+# Date: 1/23/2023
+# Program: InfoTech Center 4.0
+
+"""
+Our Welcome Screen will start our program letting
+drivers know that the InfoTech Center 4.0 OS is loading
+"""
+
+
+
+print("Welcome - InfoTech Center 4.0")
+print("InfoTech Center 4.0 is Loading")
+
+
+
+print("InfoTech Center 4.0 has loaded.")
