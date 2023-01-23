c7966a37e19a78853e50f245b825c9b03a3bfc12
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-01-23 14:42:46 -0500

    Add import & sleep w/ load message

--- DeclarationOfIndependence.py
+++ DeclarationOfIndependence.py
@@ -7,11 +7,11 @@ Our Welcome Screen will start our program letting
 drivers know that the InfoTech Center 4.0 OS is loading
 """
 
-
+import time
 
 print("Welcome - InfoTech Center 4.0")
 print("InfoTech Center 4.0 is Loading")
 
-
+time.sleep(2)
 
 print("InfoTech Center 4.0 has loaded.")
