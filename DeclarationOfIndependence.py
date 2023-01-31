6578ed8647bc0b7ddfba1a85af325d9065852d32
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-01-31 13:12:33 -0500

    Animate load message

--- DeclarationOfIndependence.py
+++ DeclarationOfIndependence.py
@@ -8,10 +8,26 @@ drivers know that the InfoTech Center 4.0 OS is loading
 """
 
 import time
+import sys
+
 
 print("Welcome - InfoTech Center 4.0")
-print("InfoTech Center 4.0 is Loading")
+print("")
 
 time.sleep(2)
 
-print("InfoTech Center 4.0 has loaded.")
+x = 0
+a = 0
+
+while x != 20:
+    x += 1
+    b = ("\033[1;33;40m InfoTech Center is Loading" + "." * a)
+    a = a + 1
+    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
+    time.sleep(0.5)
+    if a == 4:
+        a = 0
+    if x == 20:
+        print('\033[1;32;40m Complete!')
+
+
