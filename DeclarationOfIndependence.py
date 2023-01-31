240f1629cba143b668698dfc1338843dc4095a95
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-01-31 14:14:50 -0500

    Add gas alert function

--- DeclarationOfIndependence.py
+++ DeclarationOfIndependence.py
@@ -15,6 +15,7 @@ We will create a Function that will tell us out Fuel Gauge level
 """
 
 import random
+import time
 
 # Gas Level Function
 
@@ -25,9 +26,24 @@ def gasLevelGauge():
     return currentGasLevel
 
 
+# Variable calling gasLevelGauge function to store its value
+gasLevelIndicator = gasLevelGauge()
+
+# List of Gas Stations Function
+
+
 def listOfGasStations():
     gasStations = ["Shell", "Costco", "Buc-ee's", "Speedway", "7-11", "Circle-K", "Meijer", "Marathon"]
     gasStationNearby = random.choice(gasStations)
     return gasStationNearby
 
+# Determine Gas Level & Closest Station
 
+
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(1, 25), 2)
+    milesToGasStationQuarterTank = round(random.uniform(26, 50), 2)
+    if gasLevelIndicator == "Empty":
+        print("***WARNING YOU ARE ON EMPTY***")
+        time.sleep(1)
+        print("Calling emergency contact")
