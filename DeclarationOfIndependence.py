02986f474e33bcb3a4d8f14ff2fd2820b9c95974
lucascampbell04 <112645745+lucascampbell04@users.noreply.github.com>
2023-02-07 10:27:29 -0500

    Merge 'Gasoline' into

--- DeclarationOfIndependence.py
+++ DeclarationOfIndependence.py
@@ -1,33 +1,66 @@
 # Programmer: Lucas Campbell
-# Date: 1/23/2023
-# Program: InfoTech Center 4.0
+# Date: 1.31.2023
+# Program: InfoTech Center 4.0 - Gasoline
 
 """
-Our Welcome Screen will start our program letting
-drivers know that the InfoTech Center 4.0 OS is loading
+We will create a Function that will tell us out Fuel Gauge level
+- Create a List with Gas Levels
+- Randomize and choose from the list to determine our gas level
+- Create a Functon to determine our closest Gas Station
+- Create a List of gas stations
+- Randomly choose a gas station from the list.
+- Create a Function to determine our gas level and closest gas station
+- PrintGas level
+- Print Closest Gas Station
 """
 
-import time
-import sys
+import random
+from time import sleep
+
+# Gas Level Function
 
 
-print("Welcome - InfoTech Center 4.0")
-print("")
+def gasLevelGauge():
+    gasLevelList = ["Empty", "Running on Fumes", 'low', "Halfway there", "alllll good", "Full"]
+    currentGasLevel = random.choice(gasLevelList)
+    return currentGasLevel
 
-time.sleep(2)
 
-x = 0
-a = 0
+# Variable calling gasLevelGauge function to store its value
+gasLevelIndicator = gasLevelGauge()
 
-while x != 20:
-    x += 1
-    b = ("\033[1;33;40m InfoTech Center is Loading" + "." * a)
-    a = a + 1
-    sys.stdout.write('\r'+b) # \r prints a carriage return first, so `b` is printed on top of the previous line.
-    time.sleep(0.5)
-    if a == 4:
-        a = 0
-    if x == 20:
-        print('\033[1;32;40m Complete!')
+# List of Gas Stations Function
 
+
+def listOfGasStations():
+    gasStations = ["Shell", "Costco", "Buc-ee's", "Speedway", "7-11", "Circle-K", "Meijer", "Marathon"]
+    gasStationNearby = random.choice(gasStations)
+    return gasStationNearby
+
+# Determine Gas Level & Closest Station
+
+
+def gasLevelAlert():
+    milesToGasStationLow = round(random.uniform(1, 25), 2)
+    milesToGasStationQuarterTank = round(random.uniform(26, 50), 2)
+    if gasLevelIndicator == "Empty":
+        print("***WARNING YOU ARE ON EMPTY***")
+        sleep(1)
+        print("Calling emergency contact")
+    elif gasLevelIndicator == "low":
+        print("***WARNING***")
+        sleep(1)
+        print("Your gas tank is low, checking Google Maps for the closest gas station.")
+        sleep(1)
+        print("The closest gas station is", listOfGasStations(), "which is", milesToGasStationLow, "miles away.")
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank is half full witch is plenty of gas to make it to your destinations today.")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("Your gas tank is three quarters full witch is plenty of gas to make it to your destinations today.")
+    elif gasLevelIndicator == "Full Tank":
+        print("Your gas tank is full witch is plenty of gas to make it to your destinations today.")
+    else:
+        print("your gas tank is Full - Yeah! - Congrats - Vroom Vroom")
+
+gasLevelAlert()
 
