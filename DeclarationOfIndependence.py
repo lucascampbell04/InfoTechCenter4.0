7c9c54ec026493bdaea4fe58167efe869b96000c
Lucas Campbell <lucass.ccampbell@gmail.com>
2023-02-21 14:05:12 -0500

    Completed VRS

--- DeclarationOfIndependence.py
+++ DeclarationOfIndependence.py
@@ -5,6 +5,7 @@
 
 # Import Libraries here
 import random
+from time import sleep
 
 
 # Create weather conditions from in a list and choose it randomly
@@ -22,9 +23,31 @@ weatherAlert = "Snowing"
 
 # VRS() to respond to the weather conditions
 def vehicleResponseSystem():
-    if weatherAlert == "Snowing":
-        print("\nNWS has changed your alarm by 15 minutes because of the weather forecast of ", weatherAlert)
-        print("VRS has been engaged only allowing your vehicle to go 45mph")
+    if weatherAlert == "Sunshine":
+        print("\nNWS is calling for", weatherAlert, " drive safely and have a wonderful day!")
+    elif weatherAlert == "Blizzard":
+        print("\nNWS has changed your alarm for 30 minutes in advance to account for the recent", weatherAlert)
+        print("\nVRS has been engaged. Your vehicle has been limited to 35MPH for safety.")
+    elif weatherAlert == "Raining":
+        print("\nNWS is calling for", weatherAlert, " make sure to be extra careful of your surroundings.")
+    elif weatherAlert == "Foggy":
+        print("\nNWS is calling for", weatherAlert, " keep your fog lights on and drive slowly.")
+    elif weatherAlert == "Windy":
+        print("\nNWS is calling for", weatherAlert, " please be careful for flying objects.")
+    elif weatherAlert == "Icy":
+        print("\nNWS has changed your alarm for 15 minutes in advance to account for the recent", weatherAlert)
+        print("\nVRS has been engaged. Your vehicle has been limited to 45MPH for safety.")
+    elif weatherAlert == "Snowing":
+        print("\nNWS is calling for", weatherAlert," VRS has engaged and your vehicle has been limited to 45MPH for safety.")
+        print("\nVRS has been engaged. Your vehicle has been limited to 55MPH for safety.")
+    elif weatherAlert == "Cloudy":
+        print("\nNWS is calling for", weatherAlert, " drive safely and have a wonderful day!")
+    else:
+        print("\nNWS is calling for a ", weatherAlert, " your vehicle has been disabled due to natural disaster")
 
 
+# Call function here
+
+print("\nNational weather service is checking conditions")
+sleep(1)
 vehicleResponseSystem()
